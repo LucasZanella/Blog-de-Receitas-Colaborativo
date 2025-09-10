c374c801c96c1497312832f0eff2e49771954753
Lucas Zanella <lucasza.privado@gmail.com>
2025-09-09 23:06:18 -0300

    banco
    realizado a conexão ao banco e dos objetos comentário e receita

--- models/Comentario.js
+++ models/Comentario.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 
 const comentarioSchema = new mongoose.Schema({
-    texto:{
-
+    texto: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2
     },
     autor: {
-
+        type: String,
+        required: true,
+        trim: true
     },
     dataCriacao: {
-
+        type: Date,
+        default: Date.now
     },
     receitaId: {
-
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Receita',
+        required: true
     }
 });
 
--- models/Receita.js
+++ models/Receita.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
 
 const receitaSchema = new mongoose.Schema({
-    titulo:{
+    titulo: {
         type: String,
         required: true,
         lowercase: true,
         trim: true,
-        min: 4,
-        max: 35
+        minlength: 4,
+        maxlength: 35
     },
     ingredientes: {
-        
+        type: [String],
+        required: true,
+        validate: [(arr) => arr.length > 0, 'A receita deve ter ao menos 1 ingrediente']
     },
     instrucoes: {
-
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 10
     },
     autor: {
-
+        type: String,
+        required: true,
+        trim: true
     },
     categoria: {
-
+        type: String,
+        enum: ['Massas', 'Sobremesas', 'Vegano', 'Carnes', 'Saladas'],
+        required: true
     },
     dataCriacao: {
-
+        type: Date,
+        default: Date.now
     }
 });
 
